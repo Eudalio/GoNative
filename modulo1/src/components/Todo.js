779cb0933c9dc56251b1ded4381801c33ed00c1c
eudalio <tonnydallios@gmail.com>
2018-07-23 15:29:05 -0300

    Configurando defaultProps

--- modulo1/src/components/Todo.js
+++ modulo1/src/components/Todo.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
 export default class Todo extends Component {
+    static defaultProps = {
+      title: 'Todo Padrão',
+    };
+
     render() {
         return (
             <View>
