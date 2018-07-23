3e5b6701c27f9494cc4104273c9d6f882aed9a88
eudalio <tonnydallios@gmail.com>
2018-07-23 15:24:10 -0300

    Entendendo o código e Propriedades

---
+++ modulo1/src/components/Todo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default class Todo extends Component {
+    render() {
+        return (
+            <View>
+                <Text>{this.props.title}</Text>
+            </View>
+        );
+    }
+}
+
+const style = StyleSheet.create({
+
+});
