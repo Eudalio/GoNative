6a33b563b7ca624531cfa194709ea5edb2926b1e
eudalio <tonnydallios@gmail.com>
2018-07-31 13:37:59 -0300

    Validando usuário na API

--- modulo2_app_githuber/src/pages/welcome/index.js
+++ modulo2_app_githuber/src/pages/welcome/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { NavigationActions } from 'react-navigation';
 import PropTypes from 'prop-types';
+import api from 'services/api';
 
 import {
   View,
@@ -23,15 +24,35 @@ export default class Welcome extends Component {
     }).isRequired,
   };
 
-  signin = () => {
-    const resetAction = NavigationActions.reset({
-      index: 0,
-      actions: [
-        NavigationActions.navigate({ routeName: 'User' }),
-      ],
-    });
+  state = {
+    username: '',
+  };
 
-    this.props.navigation.dispatch(resetAction);
+  checkUserExists = async (username) => {
+    const user = await api.get(`/users/${username}`);
+
+    return user;
+  };
+
+  signin = async () => {
+    const { username } = this.state;
+
+    if (username.length === 0) return;
+
+    try {
+      await this.checkUserExists(username);
+
+      const resetAction = NavigationActions.reset({
+        index: 0,
+        actions: [
+          NavigationActions.navigate({ routeName: 'User' }),
+        ],
+      });
+
+      this.props.navigation.dispatch(resetAction);
+    } catch (err) {
+      // erro
+    }
   };
 
   render() {
@@ -51,6 +72,8 @@ export default class Welcome extends Component {
             autoCorrect={false}
             placeholder="Digite seu usuário"
             underlineColorAndroid="rgba(0, 0, 0, 0)"
+            value={this.state.username}
+            onChangeText={username => this.setState({ username })}
           />
 
           <TouchableOpacity style={styles.button} onPress={this.signin}>
