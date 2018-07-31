6485173c3ea35eae7eca22bfe52d325dd95de072
eudalio <tonnydallios@gmail.com>
2018-07-31 10:39:54 -0300

    Configurando navegação

--- modulo2_app_githuber/src/index.js
+++ modulo2_app_githuber/src/index.js
@@ -1,42 +1,11 @@
 import 'config/ReactotronConfig';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Routes from './routes';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+const App = () => (
+  <Routes />
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default App;
---
+++ modulo2_app_githuber/src/pages/repositories/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { View } from 'react-native';
+
+const Repositories = () => <View />;
+
+export default Repositories;
