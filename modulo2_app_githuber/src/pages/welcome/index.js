1ded50d1d95684e0653119b91cfbbf9307b7c1c5
eudalio <tonnydallios@gmail.com>
2018-07-31 11:57:11 -0300

    Estilizando welcome

--- modulo2_app_githuber/src/pages/welcome/index.js
+++ modulo2_app_githuber/src/pages/welcome/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
-const Welcome = () => <View />;
+import styles from './styles';
+
+const Welcome = () => (
+  <View style={styles.container}>
+    <Text style={styles.title}>Bem-vindo</Text>
+    <Text style={styles.text}>
+      Para continar, precisamos que você informe seu usuário no Github.
+    </Text>
+
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Digite seu usuário"
+        underlineColorAndroid="rgba(0, 0, 0, 0)"
+      />
+
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>Prosseguir</Text>
+      </TouchableOpacity>
+    </View>
+  </View>
+);
+
+Welcome.navigationOptions = {
+  header: null,
+};
 
 export default Welcome;
--- modulo2_app_githuber/src/styles/index.js
+++ modulo2_app_githuber/src/styles/index.js
@@ -2,4 +2,4 @@ import colors from './colors';
 import metrics from './metrics';
 import general from './general';
 
-export default { colors, metrics, general };
+export { colors, metrics, general };
