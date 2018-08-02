ce39339b2fae4a27ef2bcab0110381e76b19b973
eudalio <tonnydallios@gmail.com>
2018-08-02 18:06:18 -0300

    Atualizando a versão do Gradlle e adicionando ícone no header

--- modulo2_app_githuber/src/components/HeaderRight/index.js
+++ modulo2_app_githuber/src/components/HeaderRight/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
-import { TouchableOpacity, Text } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import styles from './styles';
 
 export default class HeaderRight extends Component {
   signOut = () => {
@@ -10,7 +14,7 @@ export default class HeaderRight extends Component {
   render() {
     return (
       <TouchableOpacity onPress={this.signOut}>
-        <Text>Sair</Text>
+        <Icon name="exchange" size={16} style={styles.icon} />
       </TouchableOpacity>
     );
   }
