30015efd24118d2c6f80d5c78f8c0f83c053d82c
eudalio <tonnydallios@gmail.com>
2018-08-02 23:45:39 -0300

    Consumindo API de orgs

--- modulo2_app_githuber/src/pages/organizations/index.js
+++ modulo2_app_githuber/src/pages/organizations/index.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
-
-import { View } from 'react-native';
+import api from 'services/api';
+import {
+  View,
+  Text,
+  AsyncStorage,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
+
+import styles from './styles';
 
 export default class Organizations extends Component {
   static navigationOptions = {
@@ -10,7 +18,44 @@ export default class Organizations extends Component {
     tabBarIcon: ({ tintColor }) => <Icon name="building" size={20} color={tintColor} />,
   };
 
+  state = {
+    data: [],
+    loading: true,
+  }
+
+  componentDidMount() {
+    this.loadOrganizations();
+  }
+
+  loadOrganizations = async () => {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const response = await api.get(`/users/${username}/orgs`);
+
+    this.setState({
+      data: response.data,
+      loading: false,
+    });
+  }
+
+  renderListItem = ({ item }) => <Text>{item.login}</Text>;
+
+  renderList = () => (
+    <FlatList
+      data={this.state.data}
+      keyExtractor={item => String(item.id)}
+      renderItem={this.renderListItem}
+    />
+  )
+
   render() {
-    return <View />;
+    return (
+      <View style={styles.container}>
+        { this.state.loading
+          ? <ActivityIndicator style={styles.loading} />
+          : this.renderList()
+        }
+      </View>
+    );
   }
 }
+
