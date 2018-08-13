1a453b40b2a06387dcaf2e2bcec78fb64b146adb
eudalio <tonnydallios@gmail.com>
2018-08-13 08:53:19 -0300

    Iniciando modulo 03 - Redux configurado

---
+++ modulo3_Masterizando_o_Redux/src/config/ReactotronConfig.js
@@ -0,0 +1,11 @@
+import Reactotron from 'reactotron-react-native';
+
+if (__DEV__) {
+  const tron = Reactotron.configure({ host: '192.168.0.103' }) // controls connection & communication settings
+    .useReactNative() // add all built-in react native plugins
+    .connect(); // let's connect!
+
+  tron.clear();
+
+  console.tron = tron;
+}
---
+++ modulo3_Masterizando_o_Redux/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+
+import 'config/ReactotronConfig';
+import store from 'store';
+
+const App = () => (
+  <Provider store={store}>
+    <View />
+  </Provider>
+);
+
+export default App;
