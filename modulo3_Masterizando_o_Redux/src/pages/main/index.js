bc8f1f6261b8f2083211af123043ae9da748c654
eudalio <tonnydallios@gmail.com>
2018-08-14 13:08:03 -0300

    Configurando a navegação da aplicação

---
+++ modulo3_Masterizando_o_Redux/src/pages/main/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { View } from 'react-native';
+
+const Main = () => <View />;
+
+export default Main;
---
+++ modulo3_Masterizando_o_Redux/src/routes.js
@@ -0,0 +1,9 @@
+import { StackNavigator } from 'react-navigation';
+
+import Main from 'pages/main';
+
+const Routes = StackNavigator({
+  Main: { screen: Main },
+});
+
+export default Routes;
