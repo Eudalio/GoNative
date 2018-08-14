1038c6766b232b3fab9257503c63b1c5b2bdde94
eudalio <tonnydallios@gmail.com>
2018-08-14 12:59:02 -0300

    Reestruturando para começar novo projeto

--- modulo3_Masterizando_o_Redux/src/index.js
+++ modulo3_Masterizando_o_Redux/src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 
-import './config/ReactotronConfig';
+import 'config/ReactotronConfig';
 import store from 'store';
-
-import TodoList from './TodoList';
 
 const App = () => (
   <Provider store={store}>
-    <TodoList />
+    <View />
   </Provider>
 );
 
