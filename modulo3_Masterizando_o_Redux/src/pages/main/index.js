692e36c81f0289b9aaf49cdce45460e99e39c79f
eudalio <tonnydallios@gmail.com>
2018-08-14 13:25:28 -0300

    Configurando estilos globais da aplicação

--- modulo3_Masterizando_o_Redux/src/pages/main/index.js
+++ modulo3_Masterizando_o_Redux/src/pages/main/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import {
+  View,
+  StatusBar,
+} from 'react-native';
 
-import { View } from 'react-native';
+import styles from './styles';
 
-const Main = () => <View />;
+const Main = () => (
+  <View style={styles.container}>
+    <StatusBar barStyle="light-content" />
+  </View>
+);
+
+Main.navigationOptions = {
+  header: null,
+};
 
 export default Main;
