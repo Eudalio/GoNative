f65f85f8f30f0d3e99e5f6a83ea69da781105741
eudalio <tonnydallios@gmail.com>
2018-08-15 12:51:15 -0300

    Configurando cabeçalho tela Favoritos

--- modulo3_Masterizando_o_Redux/src/routes.js
+++ modulo3_Masterizando_o_Redux/src/routes.js
@@ -1,4 +1,5 @@
 import { StackNavigator } from 'react-navigation';
+import { colors } from 'styles';
 
 import Main from 'pages/main';
 import Favorites from 'pages/favorites';
@@ -6,6 +7,14 @@ import Favorites from 'pages/favorites';
 const Routes = StackNavigator({
   Main: { screen: Main },
   Favorites: { screen: Favorites },
+}, {
+  navigationOptions: {
+    headerStyle: {
+      backgroundColor: colors.primaryDark,
+    },
+    headerTintColor: colors.white,
+    headerBackTitle: null,
+  },
 });
 
 export default Routes;
