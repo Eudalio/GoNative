b22aa7459b071f70fe6b0ba23ca5d330435b2aba
eudalio <tonnydallios@gmail.com>
2018-08-15 12:11:34 -0300

    Criado componente Main e estilizando

--- modulo3_Masterizando_o_Redux/src/pages/main/styles.js
+++ modulo3_Masterizando_o_Redux/src/pages/main/styles.js
@@ -1,11 +1,72 @@
 import { StyleSheet } from 'react-native';
 
-import { colors } from 'styles';
+import { colors, metrics } from 'styles';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.primary,
+    alignItems: 'center',
+  },
+
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: metrics.basePadding * 2,
+  },
+
+  title: {
+    fontSize: 32,
+    color: colors.white,
+    fontWeight: 'bold',
+    width: 115,
+  },
+
+  description: {
+    color: colors.white,
+    fontSize: 14,
+    lineHeight: 20,
+    marginTop: metrics.baseMargin / 2,
+    textAlign: 'center',
+  },
+
+  form: {
+    marginTop: metrics.baseMargin * 2,
+    alignSelf: 'stretch',
+  },
+
+  input: {
+    height: 50,
+    backgroundColor: colors.white,
+    borderRadius: metrics.baseRadius,
+    paddingHorizontal: metrics.basePadding,
+  },
+
+  button: {
+    height: 50,
+    backgroundColor: colors.secundary,
+    borderRadius: metrics.baseRadius,
+    paddingHorizontal: metrics.basePadding,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: metrics.baseMargin,
+  },
+
+  buttonText: {
+    color: colors.darkTransparent,
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+
+  footer: {
+    paddingBottom: metrics.basePadding,
+  },
+
+  footerLink: {
+    color: colors.white,
+    fontWeight: 'bold',
+    fontSize: 14,
   },
 });
 
