07fdcfaf4a6fe0d219b2fb27ef8456ca43768acc
eudalio <tonnydallios@gmail.com>
2018-08-15 12:37:12 -0300

    Criada rota e componente de favoritos

--- modulo3_Masterizando_o_Redux/src/routes.js
+++ modulo3_Masterizando_o_Redux/src/routes.js
@@ -1,9 +1,11 @@
 import { StackNavigator } from 'react-navigation';
 
 import Main from 'pages/main';
+import Favorites from 'pages/favorites';
 
 const Routes = StackNavigator({
   Main: { screen: Main },
+  Favorites: { screen: Favorites },
 });
 
 export default Routes;
