6ec531147b2ae0f1d59098b184338dff335cce19
eudalio <tonnydallios@gmail.com>
2018-08-15 16:39:26 -0300

    Estilizando e Renderizando as informações na tela de favoritos

---
+++ modulo3_Masterizando_o_Redux/src/pages/favorites/components/FavoriteItem/styles.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from 'react-native';
+import { colors, metrics } from 'styles';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.white,
+    padding: metrics.basePadding,
+    borderRadius: metrics.baseRadius,
+    marginTop: metrics.basePadding,
+    marginHorizontal: metrics.basePadding,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  avatar: {
+    width: 54,
+    height: 54,
+  },
+
+  info: {
+    marginLeft: metrics.baseMargin,
+    flex: 1,
+  },
+
+  title: {
+    color: colors.darker,
+    fontWeight: 'bold',
+  },
+
+  description: {
+    color: colors.dark,
+    marginTop: 3,
+    fontSize: 12,
+  }
+});
+
+export default styles;
