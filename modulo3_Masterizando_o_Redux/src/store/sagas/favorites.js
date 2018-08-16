ddffaa18a28355d9d2132ccd94267a2226db2038
eudalio <tonnydallios@gmail.com>
2018-08-16 08:54:48 -0300

    Bloqueando a inserção de repositorios repetidos

--- modulo3_Masterizando_o_Redux/src/store/sagas/favorites.js
+++ modulo3_Masterizando_o_Redux/src/store/sagas/favorites.js
@@ -1,4 +1,4 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import api from 'services/api';
 
 import { addFavoriteSucess, addFavoriteError } from 'store/actions/favorites';
@@ -7,7 +7,13 @@ export function* addFavoriteRequest(action) {
   try {
     const response = yield call(api.get, `/repos/${action.payload.repoName}`);
 
-    yield put(addFavoriteSucess(response.data));
+    const favorites = yield select(state => state.favorites.data);
+
+    if (favorites.find(favorite => favorite.id === response.data.id)) {
+      yield put(addFavoriteError('Repositório duplicado'));
+    } else {
+      yield put(addFavoriteSucess(response.data));
+    }
   } catch (err) {
     yield put(addFavoriteError('Repositório não existe'));
   }
