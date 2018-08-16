cfab5d58f2f066a995917045080d7dee5826280a
eudalio <tonnydallios@gmail.com>
2018-08-16 07:56:27 -0300

    Visualizando melhor o fluxo de informações dentro do redux saga

--- modulo3_Masterizando_o_Redux/src/config/ReactotronConfig.js
+++ modulo3_Masterizando_o_Redux/src/config/ReactotronConfig.js
@@ -1,10 +1,12 @@
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
+import sagaPlugin from 'reactotron-redux-saga';
 
 if (__DEV__) {
-  const tron = Reactotron.configure({ host: '192.168.0.100' })
+  const tron = Reactotron.configure({ host: '192.168.0.101' })
     .useReactNative()
     .use(reactotronRedux())
+    .use(sagaPlugin())
     .connect();
 
   tron.clear();
